18ee8fe6626aef12ae6c02589141aaa6d93e27dd
Gwyn Howell <gwyn.howell@appogee.co.uk>
2011-06-03 14:40:15 +0100

    fixed import error

--- src/tweetmine/cron.py
+++ src/tweetmine/cron.py
@@ -1,7 +1,6 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from tweetmine import models
-import tasks
+from tweetmine import models, tasks
 
 class CronHandler(webapp.RequestHandler):
     def get(self):
